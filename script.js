0b522243507bf315ec2d73bc3970c055f4a950fb
asiask97 <73076054+asiask97@users.noreply.github.com>
2021-02-14 21:36:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -67,28 +67,3 @@ function deletebutton(e){
     }   
 }
 
-
-
-
-
-
-       /* var value = inputvalue.value ;
-        var input = document.createElement("li");
-        input.className ='li';
-        var addingvalue = document.createTextNode(value);
-        input.appendChild(addingvalue);
-
-        var span = document.createElement('span');
-        span.setAttribute('class','delete');       
-        var svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');;
-        svg.setAttribute('width', '17');
-        svg.setAttribute('height', '18');
-        svg.setAttribute('fill', 'none');
-        svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-        path = document.createElementNS('http://www.w3.org/2000/svg',"path");  
-        path.setAttribute('d', 'M13.166 11.023l-2.844-2.606 2.62-2.86a.86.86 0 00-.05-1.215l-.632-.579a.86.86 0 00-1.214.057l-2.62 2.86L5.58 4.075a.86.86 0 00-1.214.057l-.582.636a.86.86 0 00.05 1.214l2.844 2.605-2.62 2.861a.86.86 0 00.05 1.215l.632.579a.86.86 0 001.214-.057l2.62-2.86 2.844 2.605a.86.86 0 001.215-.057l.582-.636a.86.86 0 00-.05-1.214z');
-        path.setAttribute('fill', '#ffffff');
-
-        svg.appendChild(path);
-        span.appendChild(svg);
-        input.appendChild(span);*/
